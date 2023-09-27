ec930f8512d6d2e9e39ce23fa1e0eda9087ba517
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-09-27 16:51:29 +0000

    second commit

---
+++ app/controllers/actor_controller.rb
@@ -0,0 +1,6 @@
+class ActorController < ApplicationController 
+
+  def actor
+    
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/actors", { :controller => "actor", :action => "actor" })
+
+  get("/directors", { :controller => "director", :action => "director" })
+
+  get("movies", { :controller => "movies", :action => "movies" })
+
+  get("directors/youngest", { :controller => "director", :action => "youngest" })
+
+  get("directors/eldest", { :controller => "director", :action => "eldest" })
+
 end
