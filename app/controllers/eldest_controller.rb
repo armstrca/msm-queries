a098b578f112553065a255992667447e68150aa6
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-05 21:03:50 +0000

    commit

--- app/controllers/eldest_controller.rb
+++ app/controllers/eldest_controller.rb
@@ -1,7 +1,7 @@
 class EldestController < ApplicationController
 
   def eldest
-    @old_timer = Director.order(:dob).first
+    @old_timer = Director.where.not({ :dob => nil }).order(:dob).first
     render({ :template => "misc_templates/eldest"})
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   
+  get("/directors/eldest", { :controller => "eldest", :action => "eldest" })
+  
   get("movies/", { :controller => "movies", :action => "index" })
 
   get("movies/:movie_id", { :controller => "movies", :action => "show" })
@@ -8,8 +10,6 @@ Rails.application.routes.draw do
   get("/actors", { :controller => "actor", :action => "index" })
 
   get("/actors/:actor_id", { :controller => "actor", :action => "show" })
-  
-  get("/directors/eldest", { :controller => "eldest", :action => "eldest" })
 
   get("/directors/youngest", { :controller => "youngest", :action => "youngest" })
 
